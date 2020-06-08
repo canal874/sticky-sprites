7a995ec0950cdeff3d61f2ff55de5a7de3ebb675
Hidekazu Kubota <hidekazu.kubota@gmail.com>
2020-06-08 12:34:30 +0900

    refactor: Use local parameter instead of class member

--- src/modules_main/card.ts
+++ src/modules_main/card.ts
@@ -104,18 +104,18 @@ export class Card {
     return Promise.all([this._readyToShow(), this._loadHTML(), this.loadOrCreateCardData()])
       .then(([, , _prop]) => {
         this.prop = _prop;
-        this._renderCard();
+        this._renderCard(_prop);
       })
       .catch((e: Error) => {
         throw new Error(`Cannot load card: ${this.id}: ${e.message}`);
       });
   };
 
-  private _renderCard (): void {
-    this.window.setSize(this.prop.rect.width, this.prop.rect.height);
-    this.window.setPosition(this.prop.rect.x, this.prop.rect.y);
-    logger.debug(`renderCard in main: ${JSON.stringify(this.prop.toObject())}`);
-    this.window.webContents.send('render-card', this.prop.toObject()); // CardProp must be serialize because passing non-JavaScript objects to IPC methods is deprecated and will throw an exception beginning with Electron 9.
+  private _renderCard (_prop: CardProp): void {
+    this.window.setSize(_prop.rect.width, _prop.rect.height);
+    this.window.setPosition(_prop.rect.x, _prop.rect.y);
+    logger.debug(`renderCard in main: ${JSON.stringify(_prop.toObject())}`);
+    this.window.webContents.send('render-card', _prop.toObject()); // CardProp must be serialize because passing non-JavaScript objects to IPC methods is deprecated and will throw an exception beginning with Electron 9.
     this.window.showInactive();
   }
 
